a0dc277602c96529389bc548b9dd29bc54aaf06c
yapadubpadoo <yapadubpadoo@gmail.com>
2015-10-09 12:37:55 +0700

    use instagram lib

--- src/MyInstagram.php
+++ src/MyInstagram.php
@@ -1,9 +1,15 @@
 <?php
+require_once __DIR__.'/../vendor/autoload.php';
+
+use MetzWeb\Instagram\Instagram;
 
 class MyInstagram 
 {
-
-
-
-
+	public function __construct() {
+		$this->instagram = new Instagram(array(
+		    'apiKey'      => 'YOUR_APP_KEY',
+		    'apiSecret'   => 'YOUR_APP_SECRET',
+		    'apiCallback' => 'YOUR_APP_CALLBACK'
+		));
+	}
 }
